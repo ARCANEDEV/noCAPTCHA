0d46fc3d84f873ee3bf787d2a2034eaa1cba963d
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-01-17 15:22:16 +0000

    First Commit

---
+++ tests/NoCaptchaTest.php
@@ -0,0 +1,45 @@
+<?php namespace Arcanedev\NoCaptcha\Tests;
+
+use Arcanedev\NoCaptcha\NoCaptcha;
+
+class NoCaptchaTest extends TestCase
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /** @var NoCaptcha */
+    private $noCaptcha;
+
+    const NO_CAPTCHA_CLASS = 'Arcanedev\\NoCaptcha\\NoCaptcha';
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->noCaptcha = new NoCaptcha;
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        unset($this->noCaptcha);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Test Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * @test
+     */
+    public function testCanBeInstantiated()
+    {
+        $this->assertInstanceOf(self::NO_CAPTCHA_CLASS, $this->noCaptcha);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,18 @@
+<?php namespace Arcanedev\NoCaptcha\Tests;
+
+abstract class TestCase extends \PHPUnit_Framework_TestCase
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+}
