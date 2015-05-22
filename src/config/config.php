3f8d98908f34324798d883958a30aa13b0817852
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-05-22 16:49:05 +0100

    Delete old config file

--- src/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'secret'  => getenv('NOCAPTCHA_SECRET')  ?: 'no-captcha-secret',
-    'sitekey' => getenv('NOCAPTCHA_SITEKEY') ?: 'no-captcha-sitekey',
-    'lang'    => app()->getLocale(),
-];
