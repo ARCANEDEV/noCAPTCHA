628db1a968b8fb9338c59c714ac4c2d8ab7f9230
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-04-19 13:39:28 +0100

    Adding an example for a compact captcha

---
+++ examples/compact-captcha.php
@@ -0,0 +1,26 @@
+<?php
+//
+require_once "../vendor/autoload.php";
+
+use Arcanedev\NoCaptcha\NoCaptcha;
+
+$secret  = 'your-secret-key';
+$sitekey = 'your-site-key';
+$captcha = new NoCaptcha($secret, $sitekey);
+
+if ( ! empty($_POST)) {
+    $response = $_POST['g-recaptcha-response'];
+    $result   = $captcha->verify($response);
+
+    echo $result === true ? 'Yay ! You are a human.' : 'No ! You are a robot.';
+
+    exit();
+}
+?>
+
+<form method="POST">
+    <?php echo $captcha->display(['data-size' => 'compact']); ?>
+    <button type="submit">Submit</button>
+</form>
+
+<?php echo $captcha->script(); ?>
